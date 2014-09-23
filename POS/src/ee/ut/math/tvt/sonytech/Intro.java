755257a8fa70f3b76c08d6f3f520d9344c1d6c97
unknown <prokopov@hotmail.com>
2014-09-23 11:36:39 +0300

    Initial project files. (Lab3). Feel free to edit

---
+++ POS/src/ee/ut/math/tvt/sonytech/Intro.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.sonytech;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//main that launches IntroUI.java
+	}
+
+}
---
+++ POS/src/ee/ut/math/tvt/sonytech/IntroUI.java
@@ -0,0 +1,5 @@
+package ee.ut.math.tvt.sonytech;
+
+public class IntroUI {
+	//Class that draws a window with our team description
+}
