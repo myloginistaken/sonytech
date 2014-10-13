fb33a32fa4d199d6e13736d60e53ecf15ba6b1e0
Eilyre <Eilyre@users.noreply.github.com>
2014-10-13 22:31:57 +0300

    Update Intro.java

--- POS/src/ee/ut/math/tvt/sonytech/Intro.java
+++ POS/src/ee/ut/math/tvt/sonytech/Intro.java
@@ -8,10 +8,14 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
 public class Intro {
+	
+	final static Logger logger = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
-					
+		
+		PropertyConfigurator.configure("log4j.properties");		
 		IntroUI introUI = new IntroUI();
+		introUI.logi("Ran");
 	}
 
 }
