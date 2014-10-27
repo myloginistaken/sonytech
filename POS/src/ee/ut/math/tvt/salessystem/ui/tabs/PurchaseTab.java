f5953b673a53bd71fcf164a7aff17321653e9d67
Sander <Sander@Kodu>
2014-10-27 23:53:53 +0200

    Some hack to get  History save working.

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/PurchaseTab.java
@@ -306,7 +306,6 @@ public class PurchaseTab {
   // switch UI to the state that allows to proceed with the purchase
   private void startNewSale() {
     purchasePane.reset();
-
     purchasePane.fillComboBox();
     purchasePane.setEnabled(true);
     submitPurchase.setEnabled(true);
